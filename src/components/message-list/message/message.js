817f189c97c6ebd459888e1248a3db310d1d4ed2
MariiaGeenko <88836436+MariiaGeenko@users.noreply.github.com>
2022-04-02 21:39:24 +0200

    lesson5

--- src/components/message-list/message/message.js
+++ src/components/message-list/message/message.js
@@ -1,9 +1,9 @@
-import { memo } from "react";
 import { format } from "date-fns";
 import cls from "classnames";
+import { withCounter } from "../../../hocs/with-counter";
 import styles from "./message.module.css";
 
-export const Message = memo(({ message, children }) => {
+export const Message = ({ message, children }) => {
   return (
     <div
       className={cls(styles.message, {
@@ -16,4 +16,4 @@ export const Message = memo(({ message, children }) => {
       <p>{format(message.date, "yyyy-MM-dd HH:MM:SS")}</p>
     </div>
   );
-});
+};
